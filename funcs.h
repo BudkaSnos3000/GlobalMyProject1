57505396fedf1e95cc02594a2a23d53ec98038d5
Mishqa <icontab80@Gmail.com>
2021-12-17 11:24:53 +0300

    Hi!

--- funcs.h
+++ funcs.h
@@ -1,16 +1,11 @@
+//Hi!
 // заголовочный файл funcs.h
 
-#ifndef __FUNCS_H__
 #define __FUNCS_H__
-
 double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
 double MyDiv(double, double);
-<<<<<<< HEAD
-int MyPow(int, int);
-=======
 double MyPow(double, double);
->>>>>>> develop
-
 #endif // __FUNCS_H__
+
